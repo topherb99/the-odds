624bd4ae2f143db40b7019759ed98952de0e513d
Topher <topherb99@gmail.com>
2020-02-04 16:52:23 +0000

    added: live odds from one bookmaker

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -7,10 +7,11 @@ export default class FetchData extends React.Component {
   };
 
   async componentDidMount() {
-    const url = "https://api.randomuser.me/";
+    // const url = "https://api.randomuser.me/";
+    const url = "https://api.the-odds-api.com/v3/odds/?sport=UPCOMING&region=eu&mkt=h2h&apiKey=5857547a906e62baf8fed4502a23bab0";
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({ person: data.results[0], loading: false });
+    this.setState({ match: data.data[0], loading: false });
   }
 
   render() {
@@ -18,16 +19,18 @@ export default class FetchData extends React.Component {
       return <div>loading...</div>;
     }
 
-    if (!this.state.person) {
-      return <div>didn't get a person</div>;
-    }
+    // if (!this.state.person) {
+    //   return <div>didn't get a person</div>;
+    // }
 
     return (
       <div>
-        <div>{this.state.person.name.title}</div>
-        <div>{this.state.person.name.first}</div>
-        <div>{this.state.person.name.last}</div>
-        <img src={this.state.person.picture.large} />
+        <div>{this.state.match.sport_nice}</div>
+        <br></br>
+        <div>{this.state.match.teams[0] + " VS " + this.state.match.teams[1]}</div>
+        <br></br>
+        <div>{this.state.match.sites[0].site_nice + ": " + this.state.match.sites[0].odds.h2h}</div>
+
       </div>
     );
   }
