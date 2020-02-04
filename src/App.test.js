e108f65f7c03d16814d6b79c4b5c7e1f419701e0
Topher <topherb99@gmail.com>
2020-02-04 13:23:51 +0000

    added: hello world test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('displays hello world on front page', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloWorld = getByText(/hello world/i);
+  expect(helloWorld).toBeInTheDocument();
 });
